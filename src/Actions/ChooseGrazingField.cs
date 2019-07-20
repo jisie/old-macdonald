50ebd5ae216190c27c8d899f0c83f10aeeca2e5b
Jisie DM <jisie.david@gmail.com>
2019-07-20 11:48:37 -0500

    Finished grazing field and purchasing grazing field animals

---
+++ src/Actions/ChooseGrazingField.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using Trestlebridge.Interfaces;
+using Trestlebridge.Models;
+using Trestlebridge.Models.Animals;
+
+namespace Trestlebridge.Actions {
+    public class ChooseGrazingField {
+        public static void CollectInput (Farm farm, IGrazing animal) {
+            // Console.Clear();
+
+            Console.WriteLine ("List of facilities: ");
+
+            for (int i = 0; i < farm.GrazingFields.Count; i++)
+            {
+                Console.WriteLine ($"{i + 1}. Grazing Field");
+            }
+
+            Console.WriteLine ();
+
+            // How can I output the type of animal chosen here?
+            Console.WriteLine ($"Place the animal where?");
+
+            Console.Write ("> ");
+            int choice = Int32.Parse(Console.ReadLine ()) - 1;
+
+            farm.GrazingFields[choice].AddResource(animal);
+            Console.WriteLine(farm.GrazingFields[choice]);
+
+
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
+
+        }
+    }
+}
